065ebbb8a2b36880868ab8ab21f437b900686ceb
EatMySoul <58660230+EatMySoul@users.noreply.github.com>
2020-07-26 20:36:34 +0300

    add working selector class

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from pynput import keyboard
+import os
 """
 todo:
     1) selector
@@ -6,12 +8,52 @@ todo:
 """
 class Programm():
 
-    def __init__():
-        pass
+    def __init__(self):
+        self.selector = Selector()
+        self.choose = self.selector.start()
+        print(self.choose)
+
+class Selector():
+
+    def __init__(self):
+        self.selector_data = (1,2,3,4,5)
+        self.choose_count = len(self.selector_data) - 1
+        self.selector_position = 0
+        self.selector_show()
+
+    def start(self): 
+        with keyboard.Listener(on_press=self.on_press) as listener:
+            listener.join()
+        input()
+        return self.selector_position
+
+    def on_press(self,key):
+        os.system('clear')
+        if key == keyboard.Key.up:
+            if self.selector_position == 0:
+                self.selector_position = self.choose_count
+            else:
+                self.selector_position -= 1
+        elif key == keyboard.Key.down:
+            if self.selector_position == self.choose_count:
+                self.selector_position = 0
+            else:
+                self.selector_position += 1
+        elif key == keyboard.Key.enter:
+            return False
+        self.selector_show()
+
+    def selector_show(self):
+        for i in range(self.choose_count + 1):
+            if i == self.selector_position:
+                print('\033[42m',self.selector_data[i],'\033[0m',sep='')
+            else:
+                print(self.selector_data[i])
+
 
 class Block_Scheme():
 
-    def __init__():
+    def __init__(self):
         pass
 
     def finder():
@@ -21,4 +63,7 @@ class Block_Scheme():
         pass
 
 def main():
-    pass
+    proc1 = Programm()
+
+if __name__ == "__main__":
+    main()
