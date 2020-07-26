2d2989726089bc1db9d8ba393e77b6d03ebac0a7
EatMySoul <58660230+EatMySoul@users.noreply.github.com>
2020-07-26 16:52:52 +0300

    add some function

--- main.py
+++ main.py
@@ -4,5 +4,21 @@ todo:
     2) block-schmeme of task
     3) task
 """
+class Programm():
+
+    def __init__():
+        pass
+
+class Block_Scheme():
+
+    def __init__():
+        pass
+
+    def finder():
+        pass
+
+    def adder(t):
+        pass
+
 def main():
     pass
